dcc9277c48fb0613ab86099c032e0f3177ed1b84
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2024-02-20 13:20:46 +0100

    Minor adjustments

--- meet.py
+++ meet.py
@@ -1,19 +1,17 @@
 import bs4
 import random
 import requests
-import bs4
 
 GROUP_SIZE = 3
+URL = 'https://www.novacare.no/menneskene'
 
-url = 'https://www.novacare.no/menneskene'
-response = requests.get(url)
+response = requests.get(URL)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 employee_tags = soup.find_all('h2', class_='employees__name')
-employees = [tag.text for tag in employee_tags]
-    
+employees = [tag.text for tag in employee_tags]    
 random.shuffle(employees)
 
-for i, person in enumerate(employees):
+for i, employee in enumerate(employees):
     if i % GROUP_SIZE == 0:
         print(f'\nGruppe {int(i/3+1)}:')
-    print(f'- {person}')
+    print(f'- {employee}')
